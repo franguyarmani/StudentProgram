f59d1f02f5420715d7a8480fc053be4c7ce441cf
franguyarmani <fbpeabody@gmail.com>
2018-02-20 16:47:55 -0500

    commented out rules that cause rules-based-traslator to fail

--- src/student/student.clj
+++ src/student/student.clj
@@ -1,7 +1,8 @@
 (ns student.student
     (:use clojure.walk)
     (:gen-class))
-  
+ 
+(use 'clojure.stacktrace)    
 ;; Begin Pattern Matching
 ;; ================================================================================
 (defn index-in-seq
@@ -250,19 +251,20 @@
 
 
 (def basic-student-rules 
-  '(((?x* .)                  ?x)
+  '(
+    ((?x* .)                  ?x)
     ((?x* . ?y*)          (?x ?y))
-    (('if ?x* (symbol ",") 'then ?y*)  (?x ?y))
-    (('if ?x* then ?y*)      (?x ?y))
-    (('if ?x* (symbol ",") ?y*)       (?x ?y))
-    ((?x* (symbol ",") and ?y*)      (?x ?y))
+    ; ((?x* (symbol ",") then ?y*)  (?x ?y))
+    ((if ?x* then ?y*)      (?x ?y))
+    ; ((if ?x* (symbol ",") ?y*)       (?x ?y))
+    ; ((?x* (symbol ",") and ?y*)      (?x ?y))
     ((find ?x* and ?y*)     ((= to-find-1 ?x) (= to-find-2 ?y)))
     ((find ?x*)             (= to-find ?x))
     ((?x* equals ?y*)       (= ?x ?y))
     ((?x* same as ?y*)      (= ?x ?y))
     ((?x* = ?y*)            (= ?x ?y))
     ((?x* is equal to ?y*)  (= ?x ?y))
-    ((?x* is ?y*)           (= ?x ?y))u
+    ((?x* is ?y*)           (= ?x ?y))
     ((?x* - ?y*)            (- ?x ?y))
     ((?x* minus ?y*)        (- ?x ?y))
     ((difference between ?x* and ?y*)  (- ?y ?x))
@@ -276,13 +278,14 @@
     ((?x* / ?y*)            (/ ?x ?y))
     ((?x* per ?y*)          (/ ?x ?y))
     ((?x* divided by ?y*)   (/ ?x ?y))
-    ((half ?x*)             (/ ?x 2))
-    ((one half ?x*)         (/ ?x 2))
-    ((twice ?x*)            (* 2 ?x))
+    ; ((half ?x*)             (/ ?x 2))
+    ; ((one half ?x*)         (/ ?x 2))
+    ; ((twice ?x*)            (* 2 ?x))
     ((square ?x*)           (* ?x ?x))
-    ((?x* % less than ?y*)  (* ?y (/ (- 100 ?x) 100)))
-    ((?x* % more than ?y*)  (* ?y (/ (+ 100 ?x) 100)))
-    ((?x* % ?y*)            (* (/ ?x 100) ?y))))
+    ; ((?x* % less than ?y*)  (* ?y (/ (- 100 ?x) 100)))
+    ; ((?x* % more than ?y*)  (* ?y (/ (+ 100 ?x) 100)))
+    ; ((?x* % ?y*)            (* (/ ?x 100) ?y))
+    ))
 
 
 (def ^:dynamic *student-rules* 
