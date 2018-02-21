b42df98af09a8f23c31373f7103d8cbf124b5a21
franguyarmani <fbpeabody@gmail.com>
2018-02-21 17:54:28 -0500

    added methods for list

--- src/student/ScratchPad.clj
+++ src/student/ScratchPad.clj
@@ -1,13 +1,23 @@
-(defn is-variable ; variable-p
-    [x]
-    "Is x a variable that begins with ?"
-    (and (symbol? x) (= \? (get (str x) 0))))
 
+(deftype Exp [op lhs rhs])
 
-(defn pat-match-simple [pattern input]
-    (cond (= pattern input) true
-        (or (is-variable (first pattern))(= (first pattern)(first input))) 
-                (pat-match-simple (rest pattern) (rest input))
-        :else false))
+(defmulti get-lhs class)
+(defmethod get-lhs Exp [expre]
+    (.lhs expre))
+(defmethod get-lhs clojure.lang.PersistentList [expre]
+    (second expre))
 
+(defmulti get-op class)
+(defmethod get-op Exp [expre]
+    (.op expre))
+(defmethod get-phs clojure.lang.PersistentList [expre]
+    (first expre))
 
+(defmulti get-rhs class)
+(defmethod get-rhs Exp [expre]
+    (.rhs expre))
+(defmethod get-lhs clojure.lang.PersistentList [expre]
+    (nth expre 2))
+
+(defn construct [lst]
+    (Exp. (second lst) (first lst) (nth lst 2)))
