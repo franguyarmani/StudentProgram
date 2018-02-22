d027005c691ebde9f44dac4cdd2abad44c4db184
franguyarmani <fbpeabody@gmail.com>
2018-02-22 17:58:57 -0500

    modify one and no unknown

--- src/student/ScratchPad.clj
+++ src/student/ScratchPad.clj
@@ -46,3 +46,13 @@
     (no-unknown (exp-lhs exp))(one-unknown (exp-rhs exp))
     (no-unknown (exp-rhs exp))(one-unknown (exp-rhs exp))
     :else nil))
+
+(defn unknown-parameter ;
+  "Is the argument an unknown variable?"
+  [expre]
+  (cond 
+    (= expre nil) true
+    (symbol? expre) true
+    (keyword? expre) true
+    (= '() expre) true
+    :else nil))
