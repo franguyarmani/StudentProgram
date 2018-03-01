2636e593de1024ed015f2520caf5d5b75ae69d36
franguyarmani <fbpeabody@gmail.com>
2018-03-01 14:30:51 -0500

    student v1.0

--- src/student/student.clj
+++ src/student/student.clj
@@ -384,7 +384,6 @@
 ; Utilize the word comma as our actual comma during the input phase of things
   (def ^:dynamic *basic-student-rules* 
     `[
-<<<<<<< HEAD
       ~['(?x* .)                                     '?x]
       ~['(?x* . ?y*)                            '(?x ?y)]
       ~['(if ?x* comma then ?y*)                '(?x ?y)] ;comma
@@ -420,40 +419,6 @@
       ~['(one half ?x*)                        '(/ ?x 2)]
       ~['(twice ?x*)                           '(* 2 ?x)]
       ~['(square ?x*)                         '(* ?x ?x)]
-=======
-      ~['(?x* .)                            '?x]
-      ~['(?x* . ?y*)                   '(?x ?y)]
-      ~['(if ?x* comma then ?y*)          '(?x ?y)] ;comma
-      ~['(if ?x* then ?y*)             '(?x ?y)]
-      ~['(if ?x* comma ?y*)     '(?x ?y)] ;comma
-      ~['(?x* comma and ?y*)    '(?x ?y)]   ;comma
-      ~['(?x* and ?y*)    '(?x ?y)]
-      ~['(if ?x* and ?y*)              '(?x ?y)]
-      ~['(find ?x* and ?y*)     '((= to-find-1 ?x) (= to-find-2 ?y))]
-      ~['(find ?x*)             '(= to-find ?x)]
-      ~['(?x* equals ?y*)       '(= ?x ?y)]
-      ~['(?x* same as ?y*)      '(= ?x ?y)]
-      ~['(?x* = ?y*)            '(= ?x ?y)]
-      ~['(?x* is equal to ?y*)  '(= ?x ?y)]
-      ~['(?x* is ?y*)           '(= ?x ?y)]
-      ~['(?x* - ?y*)            '(- ?x ?y)]
-      ~['(?x* minus ?y*)        '(- ?x ?y)]
-      ~['(difference between ?x* and ?y*)  '(- ?y ?x)]
-      ~['(difference ?x* and ?y*)          '(- ?y ?x)]
-      ~['(?x* + ?y*)            '(+ ?x ?y)]
-      ~['(?x* plus ?y*)         '(+ ?x ?y)]
-      ~['(sum ?x* and ?y*)      '(+ ?x ?y)]
-      ~['(product ?x* and ?y*)  '(* ?x ?y)]
-      ~['(?x* * ?y*)            '(* ?x ?y)]
-      ~['(?x* times ?y*)        '(* ?x ?y)]
-      ~['(?x* / ?y*)            '(/ ?x ?y)]
-      ~['(?x* per ?y*)          '(/ ?x ?y)]
-      ~['(?x* divided by ?y*)   '(/ ?x ?y)]
-      ~['(half ?x*)             '(/ ?x 2)]
-      ~['(one half ?x*)         '(/ ?x 2)]
-      ~['(twice ?x*)            '(* 2 ?x)]
-      ~['(square ?x*)           '(* ?x ?x)]
->>>>>>> 896d6f9eec6725f11c82613c66030a4cf6c62acd
       ~['(?x* % less than ?y*)  '(* ?y (/ (- 100 ?x) 100))]
       ~['(?x* % more than ?y*)  '(* ?y (/ (+ 100 ?x) 100))]
       ~['(?x* % ?y*)                  '(* (/ ?x 100) ?y)]
